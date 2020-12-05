94456eba8016dd88644f84ac3ea6d5df085fdc97
siya-008 <74820530+siya-008@users.noreply.github.com>
2020-12-05 18:42:44 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,34 @@
+class Box{
+    //constructor(x,y,width,height) make it generalized
+    constructor(x,y,width,height)
+   // constructor()
+    {
+        var options={
+            'restitution':0.8,
+            'friction': 0.3,
+            'density' : 1.0
+           // 'restitution': 1,
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        //this.body=Bodies.rectangle(200,100,50,50,options);
+        this.width=width
+        this.height=height
+        World.add(world,this.body);
+        
+     
+    }  
+    display(){
+      var pos=this.body.position;
+     var angle =this.body.angle;
+     push();
+      translate(pos.x,pos.y);
+     rotate(angle);
+  
+      rectMode(CENTER);
+      fill(255);
+    //  rect(pos.x,pos.y,this.width,this.height);
+    rect(0,0,this.width,this.height);
+     pop();
+  }  
+  
+  };
